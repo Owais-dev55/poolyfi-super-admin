e630f8c7ff32d8123e12f3ccc6f222b607dc058e
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2025-10-31 20:20:50 +0500

    button committed

--- src/components/CompanyManagement.tsx
+++ src/components/CompanyManagement.tsx
@@ -562,12 +562,12 @@ const CompanyManagement = () => {
                   <option value="Manufacturing">Manufacturing</option>
                   <option value="Media">Media</option>
                 </select>
-              <button className="bg-[#FFC11E] text-black px-3 py-1.5 rounded-lg text-xs font-medium hover:bg-[#E6B800] transition-colors flex items-center space-x-1.5 cursor-pointer">
+              {/* <button className="bg-[#FFC11E] text-black px-3 py-1.5 rounded-lg text-xs font-medium hover:bg-[#E6B800] transition-colors flex items-center space-x-1.5 cursor-pointer">
                 <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
                 <span>Export</span>
-              </button>
+              </button> */}
               </div>
             </div>
           </div>
